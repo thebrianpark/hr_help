a3831a02b2f10def827d791cd2368be397569d56
Brian Park <thebrianpark@gmail.com>
2022-01-17 12:03:25 -0800

    Works for multiple files

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from renamer import Renamer
+from database import EmployeeDatabase
 import os
 
 def main():
-    db = EmployeeDatabase('Employee-Tracking-Status-2022-Payroll.csv')
-    target_dir = '~/hr_stuff'
+    db = EmployeeDatabase('../Employee-Tracking-Status-2022-Payroll.csv')
+    target_dir = '/Users/brianpark/hr_stuff'
     renamer = Renamer("1.25.22", db, target_dir)
-    for file_name in os.listdir(target_dir):
+    for filename in os.listdir(target_dir):
+        print("Working on filename: {}".format(filename))
         renamer.rename_file(filename)
+
+if __name__ == '__main__':
+    main()
