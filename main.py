f34028e3b410c9ff806a8a902710cd2a6cd019bc
Brian Park <thebrianpark@gmail.com>
2022-01-17 15:23:46 -0800

    Payroll date toggle available at bootup

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from database import EmployeeDatabase
 import os
 
 def main():
+    payroll_date = input("Please enter payroll date exactly as you want to appear on the renamed files: ")
     db = EmployeeDatabase('../Employee-Tracking-Status-2022-Payroll.csv')
     target_dir = '/Users/brianpark/hr_stuff'
-    renamer = Renamer("1.25.22", db, target_dir)
+    renamer = Renamer(payroll_date, db, target_dir)
     for filename in os.listdir(target_dir):
         try:
             print("Working on filename: {}".format(filename))
